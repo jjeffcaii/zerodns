711abe96bad9ce5fb72650e4a6c57ff9f2e87c3e
Jeffsky <jjeffcaii@outlook.com>
2024-02-19 20:07:07 +0800

    chore(ci): add some ut and ignore main.rs

--- src/filter/proto.rs
+++ src/filter/proto.rs
@@ -29,3 +29,39 @@ pub trait Filter: Send + Sync + 'static {
     /// set next filter
     fn set_next(&mut self, next: Box<dyn Filter>);
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[derive(Default)]
+    struct AlwaysNoneFilter;
+
+    #[async_trait]
+    impl Filter for AlwaysNoneFilter {
+        fn next(&self) -> Option<&dyn Filter> {
+            None
+        }
+
+        fn set_next(&mut self, next: Box<dyn Filter>) {}
+    }
+
+    #[tokio::test]
+    async fn test_filter() {
+        let mut ctx = Context::default();
+        let f = AlwaysNoneFilter::default();
+
+        {
+            let mut req = Message::builder().build().unwrap();
+            assert!(f
+                .on_request(&mut ctx, &mut req)
+                .await
+                .is_ok_and(|it| it.is_none()));
+        }
+
+        {
+            let mut res = None;
+            assert!(f.on_response(&mut ctx, &mut res).await.is_ok());
+        }
+    }
+}
